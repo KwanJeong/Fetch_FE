e144a571921f2719f4a6c52e3933070f001ac627
Kwan <Jeong-Kyeongkwan@MacBook-Pro-9.local>
2023-06-21 16:02:52 -0500

    fix auth error

--- src/components/structural/App.js
+++ src/components/structural/App.js
@@ -1,54 +1,5 @@
-// import React, { useEffect, useState } from 'react';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-// import Layout from './Layout';
-// import Login from '../auth/Login';
-// import Logout from '../auth/Logout';
-// import { AuthContext } from "../contexts/AuthContext";
-// import DogsList from '../content/DogList';
-// import DogsMatch from '../content/DogsMatch';
-// import ErrorPage from '../content/ErrorPage';
-
-
-// function App() {
-
-//     // State to track the user's authentication status
-//     const [isLoggedIn, setIsLoggedIn] = useState(() => {
-//         const savedState = localStorage.getItem('isLoggedIn');
-//         if (savedState !== null) {
-//             return JSON.parse(savedState);
-//         } else {
-//             return false;
-//         }
-//     });
-
-//     useEffect(() => {
-//         // Save the authentication status to localStorage whenever it changes
-//         localStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn));
-//     }, [isLoggedIn]);
-
-
-//     return (
-//         // Provide the authentication state and setter function via context
-//         <AuthContext.Provider value={[isLoggedIn, setIsLoggedIn]}>
-//             <BrowserRouter>
-//                 <Layout>
-//                     <Routes>
-//                         <Route path="/" element={<DogsList />} />
-//                         <Route path="/match" element={<DogsMatch />} />
-//                         <Route path="/login" element={<Login />} />
-//                         <Route path="/logout" element={<Logout />} />
-//                         <Route path="/error" element={<ErrorPage />} />
-//                     </Routes>
-//                 </Layout>
-//             </BrowserRouter>
-//         </AuthContext.Provider>
-//     );
-// }
-
-// export default App;
 import React, { useEffect, useState } from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Layout from './Layout';
 import Login from '../auth/Login';
@@ -62,6 +13,20 @@ import ErrorPage from '../content/ErrorPage';
 function App() {
 
     // State to track the user's authentication status
+    // const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    //     const savedState = localStorage.getItem('isLoggedIn');
+    //     if (savedState !== null) {
+    //         return JSON.parse(savedState);
+    //     } else {
+    //         return false;
+    //     }
+    // });
+
+    // useEffect(() => {
+    //     // Save the authentication status to localStorage whenever it changes
+    //     localStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn));
+    // }, [isLoggedIn]);
+
     const [isLoggedIn, setIsLoggedIn] = useState(() => {
         const savedState = localStorage.getItem('isLoggedIn');
         if (savedState !== null) {
@@ -72,15 +37,16 @@ function App() {
     });
 
     useEffect(() => {
-        // Save the authentication status to localStorage whenever it changes
-        localStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn));
-    }, [isLoggedIn]);
-
+        const savedState = localStorage.getItem('isLoggedIn');
+        if (savedState !== null) {
+            setIsLoggedIn(JSON.parse(savedState));
+        }
+    }, []);
 
     return (
         // Provide the authentication state and setter function via context
         <AuthContext.Provider value={[isLoggedIn, setIsLoggedIn]}>
-            <HashRouter>
+            <BrowserRouter>
                 <Layout>
                     <Routes>
                         <Route path="/" element={<DogsList />} />
@@ -90,7 +56,7 @@ function App() {
                         <Route path="/error" element={<ErrorPage />} />
                     </Routes>
                 </Layout>
-            </HashRouter>
+            </BrowserRouter>
         </AuthContext.Provider>
     );
 }
