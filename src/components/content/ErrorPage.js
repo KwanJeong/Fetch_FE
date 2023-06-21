fd409a828283abde018f12a4d57ae4be775f3dde
Kwan <Jeong-Kyeongkwan@MacBook-Pro-9.local>
2023-06-21 12:41:40 -0500

    error page

---
+++ src/components/content/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <>
+            <h1>Error</h1>
+            <p>An error occurred. Please try again later.</p>
+        </>
+    );
+};
+
+export default ErrorPage;
